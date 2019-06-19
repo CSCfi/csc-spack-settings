e4e72da3433e9352506c430d4757493cc102ccc4
Sami Ilvonen <sami.ilvonen@csc.fi>
2019-06-19 12:55:16 +0300

    Summer school setup

--- test-environment/home/.bashrc
+++ test-environment/home/.bashrc
@@ -9,3 +9,11 @@ fi
 # export SYSTEMD_PAGER=
 
 # User specific aliases and functions
+source /appl/spack/run/gcc-7.3.0/lmod-7.8-7xoal6/lmod/7.8/init/profile
+source /appl/spack/run/gcc-7.3.0/lmod-7.8-7xoal6/lmod/7.8/init/lmod_bash_completions
+
+MODULEPATH_ROOT=/appl/spack/run/modulefiles/linux-ubuntu18.04-x86_64
+export MODULEPATH_ROOT=${MODULEPATH_ROOT}
+export MODULEPATH=${MODULEPATH_ROOT}/Core:${MODULEPATH_ROOT}/Linux
+export LMOD_COLORIZE=YES
+export LMOD_PREPEND_BLOCK=NORMAL
--- test-environment/run-csctestenv.sh
+++ test-environment/run-csctestenv.sh
@@ -3,18 +3,18 @@
 mynameis="$( basename "$0" )"
 myuid="$( id -u )"
 mygid="$( id -g )"
-imagename="csctestimage.simg"
+imagename="cscsummerschool19.simg"
 
 cd "$( dirname "$0" )"
 
 # Run the test environment
 if [ "$mynameis" '=' 'run-csctestenv.sh' ] ; then
     command="singularity shell -s /bin/bash --cleanenv --containall --workdir temp --bind ..:/appl --home home:/home/${USER}"
-    eval "exec ${command}" "${imagename}"
+    eval "exec ${command}" "${imagename}" "--login"
 
 # Generate a local test environment image
 elif [ "$mynameis" '=' 'create-csctestenv.sh' ] ; then
-    command="singularity build ${imagename} csc-testenv.def"
+    command="singularity build ${imagename} csc-summerschool.def"
     if [ "$UID" -eq 0 ] ; then
         eval "exec ${command}"
     else
