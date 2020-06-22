2e63992a077991853252049389f8c9e33a35c975
Sami Ilvonen <ilvonens@mahti-login3.mahti.csc.fi>
2020-06-22 13:52:55 +0300

    Removed dummy sharp dependency

--- csc-repo/packages/hcoll/package.py
+++ csc-repo/packages/hcoll/package.py
@@ -6,22 +6,19 @@
 from spack import *
 
 
-class Hcoll(Package):
+class Hcoll(AutotoolsPackage):
     """Mellanox hcoll collectives package"""
 
     homepage = "http://www.mellanox.com"
     url      = "hcoll.tar.gz"
     has_code = False
 
+    version('4.5.3045')
     version('4.4.2938')
     version('4.3.2708')
 
-    depends_on('sharp@2.0')
-    
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        spack_env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
-        run_env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
 
-    def setup_environment(self, spack_env, run_env):
-        spack_env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
-        run_env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
+    def setup_dependent_run_environment(self, env, dependent_spec):
+        env.prepend_path('LD_LIBRARY_PATH', '{0}'.format(self.prefix.lib))
